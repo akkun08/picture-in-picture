65d8863f202407df931b2acf6127db8027f8bc5c
akkun08 <akihiroaika1112@gmail.com>
2021-09-29 20:56:41 +0900

    update css create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element
+async function selectMediaStream() {
+  try {
+  } catch (error) {
+    // Catch Error Here
+    console.log("whoops, error here:", error);
+  }
+}
+
+// On load
+selectMediaStream();
