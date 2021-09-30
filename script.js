8f8c7826a04480ae43a668e0f075177e3c2bd5f8
akkun08 <akihiroaika1112@gmail.com>
2021-09-30 21:17:29 +0900

    update script.js

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const button = document.getElementById("button");
 // Prompt to select media stream, pass to video element
 async function selectMediaStream() {
   try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
   } catch (error) {
     // Catch Error Here
     console.log("whoops, error here:", error);
